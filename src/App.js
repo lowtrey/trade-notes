9b93d9a96db407faaa624c6c17a6066e28131afc
Q. Price <priceqp@gmail.com>
2019-09-18 19:55:10 -0500

    Create TradeNotes component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import TradeNotes from './TradeNotes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <TradeNotes />
     </div>
   );
 }
---
+++ src/TradeNotes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TradeNotes(props) {
+  return (
+    <div>
+      <h1>Trade Notes</h1>
+    </div>
+  );
+}
+
+export default TradeNotes;
+
