4b0d91b28d746ef434368df19c2564397b56adee
Q. Price <priceqp@gmail.com>
2019-09-19 20:44:15 -0500

    Create Footer component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from './Footer';
 import TradeNotes from './TradeNotes';
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <TradeNotes />
+      <Footer />
     </div>
   );
 }
